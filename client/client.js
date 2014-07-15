c8b684cc94e83dbc2e9a8ac1f1c105bc83b28d1e
Niko Köbler <niko@n-k.de>
2014-07-15 19:10:46 +0200

    first version / intial commit

---
+++ client/client.js
@@ -0,0 +1,42 @@
+Meteor.subscribe("mqttMessages");
+
+Template.messages.topic = function() {
+    return Session.get("topic");
+};
+
+Template.messages.lastMessages = function () {
+   return Messages.find({}, {sort: {ts: -1}});
+};
+
+Template.msg.tsString = function() {
+    return this.ts.toLocaleString();
+};
+
+Template.admin.events({
+    'click #startClient': function() {
+        Meteor.call("startClient");
+    },
+    'click #stopClient': function() {
+        Meteor.call("stopClient");
+    }
+});
+
+Template.topic.events({
+    'click #sendTopic': function() {
+        _sendTopic();
+    },
+    'keyup #topic': function(e) {
+        if (e.type == "keyup" && e.which == 13) {
+            _sendTopic();
+        }
+    }
+});
+
+_sendTopic = function() {
+    var el = document.getElementById("topic");
+    var tp = el.value;
+    Meteor.call("setTopic", tp);
+    Session.set("topic", tp);
+    el.value = "";
+    el.focus();
+};
---
+++ server/server.js
@@ -0,0 +1,47 @@
+var host = "broker.mqttdashboard.com";
+var port = 1883;
+var topic = "#";
+
+Meteor.startup(function () {
+    Messages.remove({});
+});
+
+Meteor.publish("mqttMessages", function() {
+    return Messages.find({}, {sort: {ts: -1}, limit: 10});
+});
+
+var mqtt = Meteor.require("mqtt");
+var mqttClient = mqtt.createClient(port, host);
+mqttClient
+    .on("connect", function() {
+        console.log("client connected");
+    })
+    .on("message", function(topic, message) {
+        console.log(topic + ": " + message);
+        var msg = {
+            message: message,
+            topic: topic,
+            ts: new Date()
+        };
+        addMsgToCollection(msg);
+    });
+
+var addMsgToCollection = Meteor.bindEnvironment(function(message) {
+    Messages.insert(message);
+});
+
+Meteor.methods({
+    startClient: function() {
+        console.log("startClient called");
+        mqttClient.subscribe(topic);
+    },
+    stopClient: function() {
+        console.log("stopClient called");
+        mqttClient.unsubscribe(topic);
+    },
+    setTopic: function(tp) {
+        console.log("set new Topic: " + tp);
+        mqttClient.unsubscribe(topic).subscribe(tp);
+        topic = tp;
+    }
+});
